65eba86be289f399a39c30448e6bc7d2e91cdf8e
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-02 10:17:54 -0800

    updated reduce

--- src/sumarray.js
+++ src/sumarray.js
@@ -9,12 +9,18 @@ function simpleArraySum(ar) {
 
 simpleArraySum([1,2,3,4,5,6])
 
-//using reduce 
+//using reduce method
+// a = accumulator, c = currentValue
+// a = 0, c = 1 => 1
+// a = 1, c = 2 => 3
+// a = 3, c = 3 => 6
+// a = 6, c = 4 => 10
+
 
 const numbers = [1,2,3,4]
 
-const sum = numbers.reduce((accumlator, currentValue) => {
-  return accumlator + currentValue;
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
 }, 0);
 
 sum
