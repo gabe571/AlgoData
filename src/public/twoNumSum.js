510ed211c2c20f22a422941f48f4873be51a238e
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-09 17:40:27 -0800

    working with twonumsum part 3

--- src/public/twoNumSum.js
+++ src/public/twoNumSum.js
@@ -1,8 +1,6 @@
 //double for loop to return sum of two numbers
 //example [2,7,11,15]
 
-const numbers = [2,7,11,15]
-
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++){
         const firstNum = nums[i];
@@ -16,3 +14,24 @@ var twoSum = function(nums, target) {
       return [];
   };
 
+// return the indices of two numbers too equal sum using map
+var twoSum = function(nums, target) {
+    let map = new Map;
+    for (var i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i);
+    }
+}
+
+//return indices by using a storage(comparing)
+var twoSum = function(nums, target) {
+    let storage = {};
+    
+    for(let [index, num] of nums.entries()){
+        if(storage[num] !== undefined) return [storage[num], index];
+        storage[target - num] = index;
+    }
+}
